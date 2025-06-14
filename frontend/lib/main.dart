9e6d39db4cebc73221b1d60cf5a6950031ff3c35
Sujal Rai <sujalrai@Sujals-MacBook-Air.local>
2025-06-14 21:50:14 +0545

    Update on setState

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,48 +4,38 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.greenAccent,
-          brightness: Brightness.light,
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
         ),
       ),
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Sujal's App", style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.blue,
-        ),
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.add),
-            ),
-            SizedBox(height:20.0),
-            FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.add),
-            ),
-          ],
-        ),
+        appBar: AppBar(title: Text("Flutter Practice")),
         bottomNavigationBar: NavigationBar(
           destinations: [
             NavigationDestination(icon: Icon(Icons.home), label: "Home"),
             NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
           ],
-          selectedIndex: 1,
           onDestinationSelected: (int value) {
-            print(value);
+            setState(() {
+              selectedIndex = value;
+            });
           },
+          selectedIndex: selectedIndex,
         ),
       ),
     );
