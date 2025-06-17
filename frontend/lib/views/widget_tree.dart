91b62613fd574b58585562c6b2bd15048370f41b
Sujal Rai <sujalrai@Sujals-MacBook-Air.local>
2025-06-17 14:57:41 +0545

    Update Home and Profile Nav Page

--- frontend/lib/views/widget_tree.dart
+++ frontend/lib/views/widget_tree.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/data/notifiers.dart';
 import 'package:frontend/views/pages/home_page.dart';
 import 'package:frontend/views/pages/profile_page.dart';
-import 'package:frontend/widgets/navbar_widget.dart';
+import 'package:frontend/views/widgets/navbar_widget.dart';
 
-
-List<Widget> pages = [
-  HomePage(),
-  ProfilePage(),
-];
+List<Widget> pages = [HomePage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -15,10 +12,13 @@ class WidgetTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Practice"),
-        ),
-      body: pages.elementAt(1),
+      appBar: AppBar(title: Text("Flutter Practice")),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
       bottomNavigationBar: NavbarWidget(),
     );
   }
--- frontend/lib/views/widgets/navbar_widget.dart
+++ frontend/lib/views/widgets/navbar_widget.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/data/notifiers.dart';
 
-class NavbarWidget extends StatefulWidget {
+class NavbarWidget extends StatelessWidget {
   const NavbarWidget({super.key});
 
   @override
-  State<NavbarWidget> createState() => _NavbarWidgetState();
-}
-
-class _NavbarWidgetState extends State<NavbarWidget> {
-  int selectedIndex = 0;
-  @override
   Widget build(BuildContext context) {
-    return NavigationBar(
+    return ValueListenableBuilder(
+      valueListenable: selectedPageNotifier,
+      builder: (context, selectedPage, child) {
+        return NavigationBar(
           destinations: [
             NavigationDestination(icon: Icon(Icons.home), label: "Home"),
             NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
           ],
           onDestinationSelected: (int value) {
-            setState(() {
-              selectedIndex = value;
-            });
+            selectedPageNotifier.value = value;
           },
-          selectedIndex: selectedIndex,
+          selectedIndex: selectedPage,
         );
+      },
+    );
   }
 }
